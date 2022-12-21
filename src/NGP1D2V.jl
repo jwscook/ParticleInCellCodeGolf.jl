25c02b740a041e6ddbc4d5d6a82d0b353d11ae82
James Cook <James Cook>
2022-12-21 15:25:38 +0000

    moved to basic Boris and Halton

--- src/NGP1D2V.jl
+++ src/NGP1D2V.jl
@@ -1,6 +1,16 @@
-using FFTW,LinearAlgebra,Plots,SpecialFunctions,Roots, StaticArrays, ProgressMeter
-igr(d) = 1 / Roots.find_zero(x->x^(d+1) - x - 1, 1.5)
-R(α, N, s0=0) = [rem(s0 + n * α, 1) for n in 1:N]
+using FFTW,LinearAlgebra,Plots,SpecialFunctions, StaticArrays, ProgressMeter
+
+function halton(i, base, seed=0.0)
+  result, f = 0.0, 1.0
+  while i > 0
+    f = f / base;
+    result += f * mod(i, base)
+    i ÷= base;
+  end
+  return mod(result + seed, 1)
+end
+
+
 function boris(vx, vy, E, B, dt)
   v⁻ = @SArray [vx + E * dt/2, vy, 0.0]
   t = @SArray [0, 0, B * dt/2]
@@ -8,26 +18,28 @@ function boris(vx, vy, E, B, dt)
   return (v⁺[1] + E * dt/2, v⁺[2])
 end
 function pic()
-  N=256;P=8N;T=2^13;TO=T÷16;
+  N=128;P=15N;T=2^14;TO=T÷16;
   n0=4*pi^2;vth=sqrt(n0)/N;dt=1/N/6vth;B0=sqrt(n0)/16;w=n0/P;
   @show N, P, T, TO, n0, vth, B0, dt
   @show 2pi^2 * (vth / B0)^2
-  ξ=im*zeros(N);Es=zeros(N,TO); x=(bitreverse.(0:P-1).+2.0^63)/2.0^64;
-  vx=vth * erfinv.(R(igr(1),P));vy=vth * erfinv.(R(igr(2),P));
-  r=zeros(N);E=zeros(N);X=copy(x);Vx=copy(vx);Vy=copy(vy);
+  ξ=im*zeros(N);Es=zeros(N,TO);
+  rx = halton.(0:P-1, 2, 1/sqrt(2));
+  rvx = halton.(0:P-1, 3, 1/sqrt(2));
+  rvy = halton.(0:P-1, 5, 1/sqrt(2));
+  x = rx;#(bitreverse.(0:P-1).+2.0^63)/2.0^64;
+  vx = vth * erfinv.(rvx);
+  vy = vth * erfinv.(rvy);
+  r=zeros(N);E=zeros(N);
   ik=2π*im*vcat(1,1:N/2,-N/2+1:-1);D=zeros(TO,5);
   f(g,c)=erf((g-c)*N)/2;ff(i,c)=f((i+0.5)/N,c)-f((i-0.5)/N,c);l=4eps();
-  d(c)=((mod1(i,N),ff(i,c)) for i∈(-7:7).+Int(round(c*N)));F=copy(E);
-  ρ(x,y=x)=(r.*=0;for l∈d.((x.+y)./2);for k∈l;r[k[1]]+=k[2]*w;end;end;r);
+  d(c)=((mod1(i,N),ff(i,c)) for i∈(-7:7).+Int(round(c*N)));
+  ρ(x)=(r.*=0;for l∈d.(x);for k∈l;r[k[1]]+=k[2]*w;end;end;r);
   @time @showprogress for t in 1:T;
-   X.=x;Vx.=vx;Vy.=vy;F.*=NaN;
-   for _∈0:0;
-     ≈(F,E,rtol=l,atol=0)&&break;F.=E;
-     x.=X.+(vx.+Vx)/2*dt;E.=real.(ifft((ξ.=fft(ρ(x,X))./ik;ξ[1]*=0;ξ)));
-     Base.Threads.@threads for j∈1:P;
-       Ej = sum(k->E[k[1]]*k[2], d((x[j]+X[j])/2));
-       vx[j], vy[j] = boris(Vx[j], Vy[j], Ej, B0, dt);
-     end;
+   E.=real.(ifft((ξ.=fft(ρ(x))./ik;ξ[1]*=0;ξ)));
+   Base.Threads.@threads for j∈1:P;
+     Ej = sum(k->E[k[1]]*k[2], d(x[j]));
+     vx[j], vy[j] = boris(vx[j], vy[j], Ej, B0, dt);
+     x[j] += vx[j]
    end;
    x.=mod.(x,1);
    ti = cld(t, T÷TO)
